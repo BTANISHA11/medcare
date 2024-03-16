029d68517ab123c0c9d046574b8a747c56518e65
Tushar Gagal <tushargagal1209@gmail.com>
2024-03-16 14:05:10 +0530

    ui enhancement

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
-
+import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <nav>
-      <h1>Medcare</h1>
+      <img src={logo} alt="logo" width={200} height={100} />
       <main>
         <HashLink to={"/#home"}>Home</HashLink>
         <Link to={"/contact"}>Contact</Link>
