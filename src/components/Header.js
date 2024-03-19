d2ac121818f11a175299a42fabda65bd3a50b534
TANISHA BANSAL <110708865+BTANISHA11@users.noreply.github.com>
2024-03-19 23:13:18 +0530

    Revert "authupdate"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
-import SignOut from "../SignOut";
-
+import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <nav>
-      <h1>Medcare</h1>
+      <img src={logo} alt="logo" width={200} height={100} />
       <main>
         <HashLink to={"/#home"}>Home</HashLink>
         <Link to={"/contact"}>Contact</Link>
         <HashLink to={"/#about"}>About</HashLink>
         <Link to={"/services"}>Services</Link>
-        <SignOut/>
       </main>
     </nav>
   );
