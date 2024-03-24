8e936ff0df2be7384e160ea89ec7bdb77f1e1203
Alvin <alvindennis80@gmail.com>
2024-03-24 12:19:19 +0530

    FORM QN UPDATE

--- src/components/Volunteer.jsx
+++ src/components/Volunteer.jsx
@@ -7,8 +7,7 @@ function RegistrationForm() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [confirmPassword,setConfirmPassword] = useState("");
+    const [position,setPosition] = useState("");
 
     const handleInputChange = (e) => {
         const {id , value} = e.target;
@@ -21,28 +20,22 @@ function RegistrationForm() {
         if(id === "email"){
             setEmail(value);
         }
-        if(id === "password"){
-            setPassword(value);
+        if(id === "position"){
+            setPosition(value);
         }
-        if(id === "confirmPassword"){
-            setConfirmPassword(value);
-        }
+
 
     }
 
     const submitHandler  = () => {
-        if(password === confirmPassword){
             addDoc(collection(auth, "volunteers"), {
                 firstName: firstName,
                 lastName: lastName,
                 email: email,
-                password: password
+                position:position
             });
             alert("Registration Successful");
-        }
-        else{
-            alert("Password and Confirm Password do not match");
-        }
+        
     }
 
     return(
@@ -61,12 +54,8 @@ function RegistrationForm() {
                     <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
                 </div>
                 <div className="password">
-                    <label className="form__label" for="password">Password </label>
-                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
-                </div>
-                <div className="confirm-password">
-                    <label className="form__label" for="confirmPassword">Confirm Password </label>
-                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+                    <label className="form__label" for="position">Why do you want to be a volunteer?</label>
+                    <textarea className="form__input" type="position"  id="position" value={position} onChange = {(e) => handleInputChange(e)} placeholder="Position"/>
                 </div>
             </div>
             <div class="footer">
